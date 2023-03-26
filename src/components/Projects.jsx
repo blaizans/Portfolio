3e42d7abdb2410660ec24aa3c73dcac8f379ade4
Bruno Laizans <99187215+blaizans@users.noreply.github.com>
2023-03-26 17:37:42 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 import propertyImg from '/public/rc.jpg'
 import portfolioImg from '/public/this.jpg'
+import slaughterhouse from '/public/Slaughterhouse.jpg'
 
 const Projects = () => {
   return (
@@ -18,7 +19,7 @@ const Projects = () => {
                         <h3 className='text-2xl text-white tracking-wider text-center cursor-default'>Riga Cup</h3>
                         <p className='pb-4 pt-2 text-white text-center cursor-default'>PHP/Bootstrap/MySQL/JavaScript</p>
                         <Link href="https://rigacup.lv/">
-                            <p className='text-center py-3 rounded-lg bg-white text-indigo-300 font-bold text-lg cursor-pointer'>More</p>
+                            <p className='text-center py-3 rounded-lg bg-white text-indigo-300 font-bold text-lg cursor-pointer'>Live</p>
                         </Link>
                     </div>
                 </div>
@@ -28,8 +29,19 @@ const Projects = () => {
                     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] duration-500'>
                         <h3 className='text-2xl text-white tracking-wider text-center cursor-default'>This Portfolio</h3>
                         <p className='pb-4 pt-2 text-white text-center cursor-default'>Next.JS/TailwindCSS</p>
-                        <Link href="http://www.blaizans.com/">
-                            <p className='text-center py-3 rounded-lg bg-white text-indigo-300 font-bold text-lg cursor-pointer'>More</p>
+                        <Link href="https://github.com/blaizans/Portfolio">
+                            <p className='text-center py-3 rounded-lg bg-white text-indigo-300 font-bold text-lg cursor-pointer'>Code</p>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-indigo-300 rounded-xl p-4 group hover:bg-gradient-to-r from-indigo-300 to-cyan-300'>
+                    <Image src={slaughterhouse} alt='/' className='rounded-xl group-hover:opacity-10 duration-500'/>
+                    <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] duration-500'>
+                        <h3 className='text-2xl text-white tracking-wider text-center cursor-default'>Rest API</h3>
+                        <p className='pb-4 pt-2 text-white text-center cursor-default'>Java/Springboot</p>
+                        <Link href="https://github.com/blaizans/Slaughterhouse-API">
+                            <p className='text-center py-3 rounded-lg bg-white text-indigo-300 font-bold text-lg cursor-pointer'>Code</p>
                         </Link>
                     </div>
                 </div>
