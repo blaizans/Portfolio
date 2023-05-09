83ca3f9d7854dc976023bca85d6b62183fe5acb0
Bruno Laizans <99187215+blaizans@users.noreply.github.com>
2023-05-09 13:23:02 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import propertyImg from '/public/rc.jpg'
 import portfolioImg from '/public/this.jpg'
 import slaughterhouse from '/public/Slaughterhouse.jpg'
+import synctask from '/public/synctask.png'
 
 const Projects = () => {
   return (
@@ -46,6 +47,13 @@ const Projects = () => {
                     </div>
                 </div>
 
+<div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-indigo-300 rounded-xl p-4 group hover:bg-gradient-to-r from-indigo-300 to-cyan-300'>
+                    <Image src={synctask} alt='/' className='rounded-xl group-hover:opacity-10 duration-500'/>
+                    <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] duration-500'>
+                        <h3 className='text-2xl text-white tracking-wider text-center cursor-default'>Semester Project(Ongoing)</h3>
+                        <p className='pb-4 pt-2 text-white text-center cursor-default'>.NET/Springboot/Blazor</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
